d121bf4c5aa364d3bedc3ed5ed1d3589b071b1ca
mediamanrourke <blignautr5636@my.sduhsd.net>
2026-04-21 16:58:12 +0000

    not many changes

--- script.js
+++ script.js
@@ -31,15 +31,18 @@ function myFunction(x) {
   }
 
 
-  <script>let slideIndex = 1;
+  <script>
+    let slideIndex = 1;
     showSlides(slideIndex);
     
     function plusSlides(n) {
       showSlides(slideIndex += n);
+
     }
     
     function currentSlide(n) {
       showSlides(slideIndex = n);
+
     }
     
     function showSlides(n) {
@@ -50,10 +53,13 @@ function myFunction(x) {
       if (n < 1) {slideIndex = slides.length}
       for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";  
+
       }
       for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
+
       }
       slides[slideIndex-1].style.display = "block";  
       dots[slideIndex-1].className += " active";
+      
     }</script>
