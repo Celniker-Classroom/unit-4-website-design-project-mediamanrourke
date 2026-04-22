1240f0a42e5e590193a37d4ff46c0427aefc2c24
mediamanrourke <blignautr5636@my.sduhsd.net>
2026-04-22 00:22:17 +0000

    final1

--- script.js
+++ script.js
@@ -26,23 +26,45 @@ function showSlides(n) {
 }
 
 
-function myFunction(x) {
-    x.classList.toggle("fa-thumbs-down");
+
+
+
+  let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
   }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
 
-
-  <script>
-    let slideIndex = 1;
+<script>let slideIndex = 1;
     showSlides(slideIndex);
     
     function plusSlides(n) {
       showSlides(slideIndex += n);
-
     }
     
     function currentSlide(n) {
       showSlides(slideIndex = n);
-
     }
     
     function showSlides(n) {
@@ -53,13 +75,10 @@ function myFunction(x) {
       if (n < 1) {slideIndex = slides.length}
       for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";  
-
       }
       for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
-
       }
       slides[slideIndex-1].style.display = "block";  
       dots[slideIndex-1].className += " active";
-      
     }</script>
